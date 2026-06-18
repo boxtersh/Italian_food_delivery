4f9a7811811caedcf2250f8da71002f0f7a73a8d
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2026-06-18 18:06:54 +0300

    add class OrderItem

--- models.py
+++ models.py
@@ -86,3 +86,18 @@ class Order(Base):
         "OrderItem", back_populates="order", cascade="all, delete-orphan"
     )
 
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(
+        Integer, ForeignKey("orders.id", ondelete="CASCADE"), nullable=False, index=True
+    )
+    dish_id = Column(Integer, ForeignKey("dishes.id"), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    # цена за единицу на момент оформления заказа
+    price = Column(Numeric(10, 2), nullable=False)
+
+    order = relationship("Order", back_populates="items")
+    dish = relationship("Dish")
