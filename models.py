f9233e91b6828caac9a15915677d2612dd666b95
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2026-06-18 18:03:31 +0300

    add models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+import enum
+from datetime import datetime
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Enum,
+    ForeignKey,
+    Integer,
+    Numeric,
+    String,
+    Text,
+)
+from sqlalchemy.orm import relationship
+from database import Base
