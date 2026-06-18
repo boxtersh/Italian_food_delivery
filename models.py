dd36b6a030e0c305d5f0fcd9a19b020f11eae498
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2026-06-18 18:05:02 +0300

    add class User

--- models.py
+++ models.py
@@ -29,3 +29,17 @@ class OrderStatus(str, enum.Enum):
     DELIVERING = "delivering"
     COMPLETED = "completed"
     CANCELLED = "cancelled"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    is_admin = Column(Boolean, default=False, nullable=False)
+
+    cart_items = relationship(
+        "CartItem", back_populates="user", cascade="all, delete-orphan"
+    )
+    orders = relationship("Order", back_populates="user")
