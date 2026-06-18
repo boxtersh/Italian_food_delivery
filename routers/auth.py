217402fdc9d7ecb3de6928492f45f9b0de1397f7
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2026-06-18 17:52:23 +0300

    add get_current_admin

--- routers/auth.py
+++ routers/auth.py
@@ -61,3 +61,12 @@ def get_current_user(
     if user is None:
         raise credentials_exception
     return user
+
+
+def get_current_admin(user: User = Depends(get_current_user)) -> User:
+    if not user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Требуются права администратора",
+        )
+    return user
