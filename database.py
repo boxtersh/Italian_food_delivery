2a6e2153c48ae9b7487a1bdc84c9fa1e8633ac37
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2026-06-20 16:51:16 +0300

    add comment for sqlite

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = settings.database_url
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},  # только для SQLite
+    #connect_args={"check_same_thread": False},  # только для SQLite
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
